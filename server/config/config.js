33c54f34ba8c199fa297e06a379636a88fad18e7
Hector Garcia <hectoregarciap@hotmail.com>
2018-10-03 09:38:02 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:abc123@ds121593.mlab.com:21593/cafe';
+    urlDB = process.env.MONGO_URI;
+    //urlDB = 'mongodb://cafe-user:abc123@ds121593.mlab.com:21593/cafe';
 }
 process.env.URBDB = urlDB;
